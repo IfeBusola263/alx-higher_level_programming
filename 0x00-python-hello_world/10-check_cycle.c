5ca97c25cd2c1e3eda67d970a553dca287ce7687
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-05 21:45:00 +0000

    Check for cycle in singly linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * check_cycle - checks for loop in linked list
+ * @list: head pointer to linked list
+ *
+ * Return: 0 if none, 1 if there is
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tort, *hare;
+
+	if (list != NULL)
+	{
+		tort = list;
+		hare = list;
+
+		while (hare->next && hare && hare->next->next)
+		{
+			hare = hare->next->next;
+			tort = tort->next;
+			if (hare == tort)
+			{
+				return (1);
+			}
+		}
+		return (0);
+	}
+	return (-1);
+}
