77416c4b35ee880ce1b22647276bcc3a2743eecd
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-06 21:19:54 +0000

    Add two numbers

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    ''' Returns the addition of a and b.
+
+    >>> add(40, 10)
+    50
+    >>> add(-10, 20)
+    10
+    >>> add(200, -65)
+    135
+    '''
+    return a + b
