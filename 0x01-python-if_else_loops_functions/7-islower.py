4534df35b49b82a9e45d9e1119d4de2094445f19
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-06 20:21:27 +0000

    Check if character is lower or Upper case

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,13 @@
+def islower(c):
+    """ Returns True if character c is lower and false otherwise
+    >>> islower("a")
+    'True'
+    >>> islower("A")
+    'False'
+    >>> islower(90)
+    'False'
+    """
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
