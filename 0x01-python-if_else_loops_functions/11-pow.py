60ce02c6d38b52bea16bce34091ffc344785b1f4
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-06 21:29:58 +0000

    Get value of quotient raise to a exponent

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def pow(a, b):
+    ''' Return the value of a to the power of b
+
+    >>> pow(2, 2)
+    4
+    >>> pow(98, 2)
+    9604
+    >>> pow(98, 0)
+    1
+    >>> pow(100, -2)
+    0.0001
+    '''
+    return a ** b
