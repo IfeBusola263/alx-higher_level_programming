efbedc40d5c000fdca345a4df7e0a0b4a0b0f1c4
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-07 00:05:56 +0000

    Print strings in Upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -9,10 +9,13 @@ def uppercase(str):
     '''
 # Loop through each character
     for i in range(len(str)):
-# if the character is within the small letter ascii alphabets
+
+        # if the character is within the small letter ascii alphabets, convert.
         if ord(str[i]) >= 97 or ord(str[i]) <= 122:
-# convert it to capital letter and print it
+
+            # convert it to capital letter and print it
             print("{:c}".format(ord(str[i]) - 32), end='')
-# if it is not print it as is
+
+        # if it is not print it as is
         else:
             print("{}".format(str[i]), end='')
