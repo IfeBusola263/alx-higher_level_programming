a64fdcec3b7071531aae447688a666886a9285ab
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-08 15:43:01 +0000

    Load variable from module and display value

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Check if this is the main program
+if __name__ == "__main__":
+
+    # Bring load the specific variable from the module
+    from variable_load_5 import a
+
+    # display the value of the variable
+    print(a)
