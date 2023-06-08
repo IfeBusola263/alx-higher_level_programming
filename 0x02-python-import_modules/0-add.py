2338fa481cb2c1ff621c5089d592625b7a0d4185
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-08 13:10:20 +0000

    Print addition of numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,6 @@
 from add_0 import add as ad
 
 if __name__ == "__main__":
-    import sys
-    if sys.argv(0) == "__import__":
-        pass
-    else:
-        a = 1
-        b = 2
-        print("{} + {} = {}".format(a, b, ad(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, ad(a, b)))
