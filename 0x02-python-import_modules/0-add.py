b96e20132fedbbb91c3983dc20228d76af255db6
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-08 19:13:13 +0000

    correct 0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,4 @@ from add_0 import add as ad
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{0:d} + {1:d} = {2:d}".format(a, b, ad(a, b)))
+    print("{0} + {1} = {2}".format(a, b, ad(a, b)))
