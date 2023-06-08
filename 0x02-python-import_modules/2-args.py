8f9e62f1358c5df1fc0ca0550273af0e7ffb7e5b
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-08 14:16:50 +0000

    Print number of and comand line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# check if it's the main program
+if __name__ == '__main__':
+    # Bring in right module for command line args
+    import sys
+
+    # check if there no argument
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+
+    # check if there's just one argument
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(1))
+        for i in range(1,len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
+
+    # check for more than one argument
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1,len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
