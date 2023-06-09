4bb65cebb4e48a4f149841252b61f053b3509ad3
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-09 20:41:47 +0000

    Print the integers in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    ''' list -> int
+    
+    Prints the content of the string.
+
+    >>> print_list_integer([1, 2, 3, 4, 5])
+    1
+    2
+    3
+    4
+    5
+    '''
+    for item in my_list:
+        print("{:d}".format(item))
