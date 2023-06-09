fb2ebb0e02729abbf5c05149ffda5b314002e0df
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-09 22:21:11 +0000

    Print list inreverse order

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    ''' List -> (int)
+
+    Print item of the list 'my_list' in reverse order.
+
+    >>> print_reversed_list_integer([1, 2, 3, 4, 5])
+    5
+    4
+    3
+    2
+    1
+
+    '''
+    # confirm it is not an empty list
+    if my_list is not []:
+
+        # Reverse the list
+        my_list.reverse()
+
+        # loop through the list to print each item
+        for item in my_list:
+            print("{:d}".format(item))
