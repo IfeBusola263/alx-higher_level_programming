c924be403b8e784134dab1c0920779c8dfa68210
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-10 18:35:01 +0000

    The Process removes item at a specified index in a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,27 @@
+#!/Usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    ''' List -> None
+    Mutate the list 'my_list' deleting the item at index 'idx'.
+    if index idx is negative or out of range, do nothing
+
+    >>> delete_at([1, 2, 3, 4, 5], 3)
+    [1, 2, 3, 5]
+    >>> delete_at([1, 2, 3, 4, 5], -3)
+    [1, 2, 3, 4, 5]
+    >>> delete_at([1, 2, 3, 4, 5], 10)
+    [1, 2, 3, 4, 5]
+
+    '''
+
+    # Check if it's not an empty list
+    if my_list is not None:
+
+        # remove the item at the specified index
+        del my_list[idx]
+
+        # return the modified list
+        return my_list
+
+    # return None if the list is empty
+    else:
+        return
