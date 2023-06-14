5521a136ffc434567eb9d98b2f46aef1c3b70e26
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-14 14:19:38 +0000

    Get the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    ''' Returns the number of keys in a dictionary 'a_dictionary'
+
+    >>> a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
+    >>> number_keys(a_dictionary)
+    3
+
+    '''
+
+    return len(a_dictionary)
