9c9814ce14138c1e01e229409ed37590488beca3
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-14 20:46:03 +0000

    Get weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -9,6 +9,6 @@ def weight_average(my_list=[]):
             a, b = item
             tup_add += (a * b)
             weight_add += b
-        return tup_add/ weight_add
+        return tup_add / weight_add
     else:
         return 0
