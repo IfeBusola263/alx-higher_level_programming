a8931c8e137dcc3eaecc7e462f6216f191037d72
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-14 15:42:55 +0000

    Use map to perform multiplication operation

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    ''' Return a list 'my_list' with all the elements nultiplied by 'number'
-
-    >>> my_list = [1, 2, 3, 4, 6]
-    >>> multiply_list_map(my_list, 4)
-    [4, 8, 12, 16, 24]
-
-    '''
-    new_list = list(map(lambda x: x * number, my_list))
-
-    return new_list
+    return list(map(lambda x: x * number, my_list))
