4bfe278872048f22b2ef1223312b7aa43a30cf86
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-14 22:48:36 +0000

    Get Maximum integer

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -11,17 +11,17 @@ def best_score(a_dictionary):
 
     '''
 
-    max_list = ""
     # if the dictionary is an empty dictionary
     # otherwise find the maximum value
     if a_dictionary is not None:
         val_list = a_dictionary.values()
+        if val_list is None:
+            return None;
         max_val = max(val_list)
 
         # loop the dictionary to match the value with max_val to return key
         for key, value in a_dictionary.items():
             if value == max_val:
-                max_list += key
-        return max_list
+                return key
     else:
         return None
