9f58360abb1e889a448429faa3d79f3a7ae78287
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-14 23:04:04 +0000

    New corrections

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,11 @@
 #include "Python.h"
 #include <stdio.h>
-
+/**
+ * print_python_list - print python list info
+ * @p: pointer to the object
+ *
+ * Return: nothing
+ */
 void print_python_list(PyObject *p)
 {
 	PyListObject *list;
@@ -17,7 +22,12 @@ void print_python_list(PyObject *p)
 		printf("Element %d: %s\n", i, list->ob_item[i]->ob_type->tp_name);
 	}
 }
-
+/**
+ * print_python_bytes - print information on strings
+ * @p: pointer to the python object
+ *
+ * Return: nothing
+ */
 void print_python_bytes(PyObject *p)
 {
 	PyBytesObject *by;
@@ -25,7 +35,7 @@ void print_python_bytes(PyObject *p)
 	by = (PyBytesObject)p;
 
 	printf("[.] bytes object info\n");
-	printf("  size: %d\n", p->ob_base->ob_size);
+	printf("  size: %lu\n", by->ob_base->ob_size);
 	/* printf("  trying string: %s\n", ); */
 	/* printf("  first %d bytes: \n", ); */
 }
