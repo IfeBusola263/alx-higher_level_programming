c130cd449ba33184356913f978140adef3f26e8c
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-14 23:09:26 +0000

    New corrections

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -14,10 +14,10 @@ void print_python_list(PyObject *p)
 	list = (PyListObject)p;
 
 	printf("[*] Python List info\n");
-	printf("[*] Size of the Python List = %lu\n", list->ob_base->ob_size);
+	printf("[*] Size of the Python List = %lu\n", list->ob_base.ob_size);
 	printf("[*] Allocated = %lu\n", list->allocated);
 
-	for (i = 0; i < list->ob_base->ob_size; i++)
+	for (i = 0; i < list->ob_base.ob_size; i++)
 	{
 		printf("Element %d: %s\n", i, list->ob_item[i]->ob_type->tp_name);
 	}
