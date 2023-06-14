32208b4d10ed533b1648c8af7e0c48c5485aa010
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-14 15:23:07 +0000

    Get the maximum value of a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    ''' Returns the key with the highest value in a dictionary
+
+    >>> a_dictionary = {'John': 12, 'Bob': 14,
+    'Mike': 14, 'Molly': 16, 'Adam': 10}
+    >>> best_score(a_dictionary)
+    >>> Molly
+    >>>
+    >>> best_score(None)
+
+    '''
+
+    # if the dictionary is an empty dictionary
+    if a_dictionary is None:
+        return None
+
+    # otherwise find the maximum value
+    else:
+        val_list = a_dictionary.values()
+        max_val = max(val_list)
+
+        # loop the dictionary to match the value with max_val to return key
+        for key, value in a_dictionary.items():
+            if value == max_val:
+                return key
