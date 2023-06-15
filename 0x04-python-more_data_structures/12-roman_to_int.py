68d9487ff6ea83d83172676eb77571a103b40494
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-15 06:08:30 +0000

    Roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-from functools import reduce
-
 
 def roman_to_int(roman_string):
     ''' Return the coversion of a roman numeral instance '''
+
+    ret = 0
     if type(roman_string) is str:
         rom = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
         pattern = [rom.get(item) for item in roman_string]
-        return reduce(lambda x, y: x if x > y else y - x, pattern)
+        for item in range(1, len(pattern)):
+            if pattern[item - 1] < pattern[item]:
+                pattern[item] = pattern[item] - pattern[item - 1]
+                pattern[item - 1] = 0
+        return sum(pattern)
     else:
         return 0
