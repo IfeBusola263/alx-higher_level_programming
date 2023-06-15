ffc5fa61649e971004c19c296cb2d7706c46f3b4
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-15 05:07:46 +0000

    Roman numerals cor

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ from functools import reduce
 
 
 def roman_to_int(roman_string):
-
+    ''' Return the coversion of a roman numeral instance '''
     if type(roman_string) is str:
         rom = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
         pattern = [rom.get(item) for item in roman_string]
