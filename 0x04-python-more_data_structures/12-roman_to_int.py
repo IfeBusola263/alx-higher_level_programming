3f6e71c3504e671fee716660e6b13b7524c275e1
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-15 04:54:15 +0000

    Roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,10 +6,7 @@ def roman_to_int(roman_string):
 
     if type(roman_string) is str:
         rom = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-
         pattern = [rom.get(item) for item in roman_string]
-
         return reduce(lambda x, y: x if x > y else y - x, pattern)
-
     else:
         return 0
