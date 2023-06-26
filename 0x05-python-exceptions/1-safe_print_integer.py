376a2a9ecba0cbf44bb9744acc2d74323094826d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-26 13:02:42 +0000

    Print integer and returns false if not an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    ''' Return True if an integer has been correctly printed
+    and false otherwise
+
+    >>> safe_print_integer(89)
+    89
+    >>> safe_print_integer(-89)
+    -89
+    >>> safe_print_integer("school")
+    False
+
+    '''
+    try:
+        print("{:d}".format(int(value)))
+        return True
+
+    except Exception as err:
+        return False
