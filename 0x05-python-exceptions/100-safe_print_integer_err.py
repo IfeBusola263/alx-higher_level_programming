76ce25dd862bcae54fbdeb02295b1b875f70c51d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-26 16:21:28 +0000

    Print output to standard error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    ''' Return True if an integer has been correctly printed
+    and false otherwise
+
+    >>> safe_print_integer(89)
+    89
+    >>> safe_print_integer(-89)
+    -89
+    >>> safe_print_integer("school")
+    False
+
+    '''
+    try:
+        print("{:d}".format(value))
+        return True
+
+    except Exception as err:
+        print("Exception:", err, file=sys.stderr)
+        return False
