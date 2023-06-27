2c92063284a6f3ea65504403488463157f1625f7
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-27 22:37:38 +0000

    Caught Exceptions for size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,8 +9,8 @@ class Square:
         ''' Initializes the file attribute '''
         self.__size = size
 
-        try:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-        except TypeError:
-            print("size must be an integer")
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
