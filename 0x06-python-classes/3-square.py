f182acaa3346fcd24f0a2f98845ff59161b68569
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-27 21:50:13 +0000

    Return area of the square with method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' This module contains a class, with field size '''
+
+
+class Square:
+    ''' This class instantiates the field '''
+
+    def __init__(self, size=0):
+        ''' Initializes the file attribute '''
+        self.__size = size
+
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            print("size must be an integer")
+
+    def area(self):
+        ''' Returns the area of the square. '''
+        return self.__size ** 2
