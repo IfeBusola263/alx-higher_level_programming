e18d5fabec44bb4fcf7d366770e92da98ed90cf8
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-27 21:10:08 +0000

    Instantiate a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' This module contains a class, with field size '''
+
+
+class Square:
+    ''' This class instantiates the field '''
+
+    def __init__(self, size):
+        ''' Initializes the file attribute '''
+        self.__size = size
