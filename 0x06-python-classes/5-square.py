92ab9a8d81c34c8749071d68689334eb4c716b5a
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-27 22:53:42 +0000

    Print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+''' This module contains a class, with field size '''
+
+
+class Square:
+    ''' This class instantiates the field '''
+
+    def __init__(self, size=0):
+        ''' Initializes the file attribute '''
+        self.size = size
+
+    @property
+    def size(self):
+        ''' This is a pythonic way to protect to variable size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' Sets the value if any wrong input is given by user '''
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        ''' Returns the area of the square. '''
+        return self.__size ** 2
+
+    def my_print(self):
+        ''' This method prints the square with the character #'''
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print('#', end='')
+            print()
