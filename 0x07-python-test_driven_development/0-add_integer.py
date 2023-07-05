0925a1c3ff59b626b5b7f63e483ea3f2e107d3f0
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-05 10:36:49 +0000

    Test file for add integer program

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,9 @@
 def add_integer(a, b=98):
     ''' Returns the addition of two integers a and b '''
 
+    if a is None:
+        raise TypeError("a must be an integer")
+    
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
 
