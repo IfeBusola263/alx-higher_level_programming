26a56486b31e42097328dd8917fd21ea95c68dd8
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-05 09:47:04 +0000

    Test file for add integer program

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 ''' This module houses a function that adds an integer '''
 
+
 def add_integer(a, b=98):
     ''' Returns the addition of two integers a and b '''
-
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
 
@@ -11,6 +11,7 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
+
 
 if __name__ == '__main__':
     import doctest
