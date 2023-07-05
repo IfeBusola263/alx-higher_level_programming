7c48966b21145ed657023f783cd71a89c700cae8
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-05 10:40:19 +0000

    Test file for add integer program

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,7 +7,7 @@ def add_integer(a, b=98):
 
     if a is None:
         raise TypeError("a must be an integer")
-    
+
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
 
@@ -15,6 +15,7 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
+
 
 if __name__ == '__main__':
     import doctest
