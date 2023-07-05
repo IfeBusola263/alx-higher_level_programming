bb672753521ce7ebcf313acbb6690593210bf2f4
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-05 15:35:51 +0000

    Matrix Function update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,11 +7,13 @@ def matrix_divided(matrix, div):
     divided by div'''
 
     main_list = []
-    if type(matrix) is not list or len(matrix)< 2:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(matrix) is not list or type(div) is None:
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
 
-    if matrix == None or div == None:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(matrix[0]) is not list or matrix is None or div is None:
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
 
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
@@ -19,17 +21,26 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
+    row = len(matrix[0])
+
     for item in matrix:
         inner_list = []
 
         if type(item) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix "
+                            "(list of lists) of integers/floats")
+
+        if len(item) != row:
+            raise TypeError("Each row of the matrix must have the same size")
+        else:
+            row = len(item)
 
         for num in item:
             if type(num) in [int, float]:
                 inner_list.append(round(num/div, 2))
             else:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                                "(list of lists) of integers/floats")
         main_list.append(inner_list)
 
     return main_list
