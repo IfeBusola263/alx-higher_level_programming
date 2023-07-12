2e353c0f48ac9c068de785e28092795cd0dd984b
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-12 03:05:56 +0000

    Corrected 10-square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,47 +2,49 @@
 ''' This module implements a Base Geometry class'''
 
 
-class BaseGeometry:
-    ''' This is an empty class '''
+# class BaseGeometry:
+#     ''' This is an empty class '''
 
-    def __init__(self):
-        ''' initiatized when an instance of the class is created '''
-        pass
+#     def __init__(self):
+#         ''' initiatized when an instance of the class is created '''
+#         pass
 
-    def area(self):
-        ''' raises an exception when called '''
-        raise Exception("area() is not implemented")
+#     def area(self):
+#         ''' raises an exception when called '''
+#         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        ''' validates the type of object value and name '''
+#     def integer_validator(self, name, value):
+#         ''' validates the type of object value and name '''
 
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+#         if type(value) is not int:
+#             raise TypeError(f"{name} must be an integer")
 
-        if value <= 0:
-            raise ValueError(f'{name} must be greater than 0')
+#         if value <= 0:
+#             raise ValueError(f'{name} must be greater than 0')
 
 
-class Rectangle(BaseGeometry):
-    ''' A class that inherirs from BaseGeometry '''
+# class Rectangle(BaseGeometry):
+#     ''' A class that inherirs from BaseGeometry '''
 
-    def __init__(self, width, height):
-        ''' the init magic method is initiated when an
-        instance of the class is created'''
+#     def __init__(self, width, height):
+#         ''' the init magic method is initiated when an
+#         instance of the class is created'''
 
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
+#         BaseGeometry.integer_validator(self, "width", width)
+#         BaseGeometry.integer_validator(self, "height", height)
 
-        self.__width = width
-        self.__height = height
+#         self.__width = width
+#         self.__height = height
 
-    def area(self):
-        ''' Returns the area of the rectangle '''
-        return self.__width * self.__height
+#     def area(self):
+#         ''' Returns the area of the rectangle '''
+#         return self.__width * self.__height
 
-    def __str__(self):
-        ''' Returns the string representation of the area, formatted '''
-        return "[Rectangle]" + " " + f"{self.__width}/{self.__height}"
+#     def __str__(self):
+#         ''' Returns the string representation of the area, formatted '''
+#         return "[Rectangle]" + " " + f"{self.__width}/{self.__height}"
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
