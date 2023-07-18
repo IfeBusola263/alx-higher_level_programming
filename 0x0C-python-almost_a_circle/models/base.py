9bed10d1e4dec87ddfd35752806e5ad5aa7e5789
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-18 23:24:33 +0000

    Implemented the static class for JSON conversion

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 ''' This module implements a class Base '''
 
+import json
+
 
 class Base:
     ''' This class base class of all other class '''
@@ -20,3 +22,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' The method converts python objects to JSON
+        string for parsing '''
+
+        if list_dictionaries is None:
+            return '[]'
+        if isinstance(list_dictionaries, list) and len(list_dictionaries) < 1:
+            return '[]'
+
+        return json.dumps(list_dictionaries, sort_keys=True)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,7 +4,7 @@ Base class and Rectangles class and subclass'''
 
 import unittest
 from models.base import Base
-
+from models.rectangle import Rectangle
 
 class TestBase(unittest.TestCase):
     ''' This tests the class Base '''
@@ -33,3 +33,18 @@ class TestBase(unittest.TestCase):
 
         with  self.assertRaises(ValueError):
             self.obj3 = Base(-12000)
+
+    def test_to_json_string(self):
+        ''' Test for the static method to_json '''
+
+        expect_string = '[{"height": 2, "id": 15, "width": 5, "x": 3, "y": 4}]'
+        obj = Rectangle(5, 2, 3, 4, 15)
+        dict_rep = obj.to_dictionary()
+        json_rep = Base.to_json_string([dict_rep])
+        self.assertEqual(json_rep, expect_string)
+
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+        with self.assertRaises(TypeError):
+            js_rep = Base.to_json_string()
