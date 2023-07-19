17246827d8a49291d970ab769a20cce23ca1787e
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-19 01:11:59 +0000

    Implemented the safe to file class method for generating json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 ''' This module implements a class Base '''
 
 import json
+import os
 
 
 class Base:
@@ -34,3 +35,43 @@ class Base:
             return '[]'
 
         return json.dumps(list_dictionaries, sort_keys=True)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''' The saves the json representation of a python object
+        in a file '''
+
+        # open file for writing, first check if the file exists
+
+        insts = ["Rectangle", "Square"]
+
+        if list_objs is not None:
+
+            # get dictionary representation of model
+            js_dict = []
+            for cls in list_objs:
+                js_dict.append(cls.to_dictionary())
+
+            for inst in insts:
+
+                # check the class to work with
+                if inst in str(type(list_objs[0])):
+
+                    j_file = "./" + inst + ".json"
+                    oj_file = inst + ".json"
+                    # check if the file exists
+                    if os.path.exists(j_file):
+
+                        # open the file for writing
+                        with open(oj_file, "w", encoding="utf-8") as file:
+
+                            file.write(Base.to_json_string(js_dict))
+
+                    # If the file does not exist
+                    else:
+                        with open(oj_file, "w", encoding="utf-8") as file:
+                            file.write(Base.to_json_string(js_dict))
+
+        else:
+            with open("None.json", "w", encoding="utf-8") as file:
+                file.write('[]')
