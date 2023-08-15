b4e6dd73874ab8b15dabb004814fdde2c8171bce
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-15 20:29:48 +0000

    Displays the full description of the first_table table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- This script displays the description of the table first table
 -- The syntax below describes the table 'first_table'
-SHOW COLUMNS FROM first_table;
+SHOW CREATE TABLE first_table;
