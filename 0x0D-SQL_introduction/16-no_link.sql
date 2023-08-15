43cfcbdb706a8e005090c3e461490018580d6275
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-15 21:48:33 +0000

    Displays content of Database

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,3 @@
 -- The Script displays all the entry in the database except rows with name as NULL
 -- The syntax displays all the entires in the table
-SELECT score, name  FROM second_table
-WHERE name IS NOT NULL GROUP BY score ORDER BY score DESC;
+SELECT score, name  FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
