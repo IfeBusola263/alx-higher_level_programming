f304064920acd25b28d259686576be0eb44b1b86
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-15 21:08:49 +0000

    Displays the content of the table in descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Displaying the contents of the table second_table, just the score
+-- and name fields with the output arranged in Descending order
+SELECT score, name FROM second_table ORDER BY score DESC;
