566a91bc82c31c8408fff8d0e85dcb31a93ea1bf
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-15 21:00:00 +0000

    Script creates table and multiple insertion

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- The creates a table and adds multiple rows
+-- The syntax create the table but ensures no failure if it already exists
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES(1, "John", 10);
+INSERT INTO second_table VALUES(2, "Alex", 3);
+INSERT INTO second_table VALUES(3, "Bob", 14);
+INSERT INTO second_table VALUES(1, "George", 8);
