eb2ae1cbb3722efba3d0ada1809d6db86d25c148
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-15 21:11:34 +0000

    Displays the contents in descending order of the scores for scores >= 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Displaying the contents of the table second_table, just the score
+-- and name fields with the output arranged in Descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
