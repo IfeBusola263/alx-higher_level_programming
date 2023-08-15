065e7b92a0099ba696217006734f2d02d5c52c50
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-15 19:46:46 +0000

    Script to delete the database

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- I need to delete a database if it exists
+-- The syntax below deletes the database hbtn_0c_0 if it exists
+-- and does not fail if it doesn't exist
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
