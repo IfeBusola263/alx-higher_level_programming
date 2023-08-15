6e1c1ba7cf431d3fb5d8aa104dfdc4a03db6200c
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-15 21:35:03 +0000

    Counts the number of entries with similar score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- The script displays the scores and number of entires with the score
 -- The syntax counts the number of entries with the same score
-SELECT score, COUNT(score) AS "number" FROM second_table ORDER BY score DESC;
+SELECT score, COUNT(score) AS "number" FROM second_table GROUP BY score DESC;
