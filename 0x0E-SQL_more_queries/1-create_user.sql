dbfb00d3a12b352abc923c18548694562115f21f
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-16 20:38:40 +0000

    Correct Root User

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- The Script creates a new User and sets the privileges
 -- The syntax create a new user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
