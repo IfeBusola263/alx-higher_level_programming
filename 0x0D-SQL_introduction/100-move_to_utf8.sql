b48a3e6c8a2465765a61819c23c4b2cfba422b61
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-16 19:37:58 +0000

    Convert to UTF-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,5 @@
 -- The script converts the the database to UTF-8
 -- the syntax below converts the DB, Table and name column to UTF-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- IF EXISTS (SELECT * FROM information_schema.tables
--- WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table') THEN
-ALTER TABLE first_table CONVERT TO CHARACTER
-SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY
-name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- END IF;
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CONVERT TO  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
