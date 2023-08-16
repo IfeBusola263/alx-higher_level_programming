88134c403cb1872997ad300ded69a1366db77e3f
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-16 20:44:27 +0000

    Create user and set privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- The script Creates a new database and User
 -- The password is set and ensures that it does not fail if the DB exists already
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
