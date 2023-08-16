90da6a773bbed1cbe7381b8616b92b9603cb21bb
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-16 04:51:50 +0000

    Finds Maximum of States

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 -- The script finds the average of temperatures
 -- The syntax below finds the average of temperature of multiple
 -- entires of different cities
-SELECT state, MAX(value) AS "max_temp" FROM temperatures GROUP BY state;
+SELECT state, MAX(value) AS "max_temp" FROM temperatures GROUP BY state, ORDER BY state;
