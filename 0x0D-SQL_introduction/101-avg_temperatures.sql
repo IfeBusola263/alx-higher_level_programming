30e6db816a1b3ba8a30ea3265f4032836525ffd3
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-16 04:20:27 +0000

    Finds Average of the temperatures

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- The script finds the average of temperatures
+-- The syntax below finds the average of temperature of multiple
+-- entires of different cities
+SELECT city AVG(value) AS "avg_temp" FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
