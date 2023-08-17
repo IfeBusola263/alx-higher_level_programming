8e3ffdfa818e8e0ed5c26d20bc8c3343bf4460ef
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-17 05:23:48 +0000

    Join without the intersection

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- The scripts displays all the shows in the database linked to a genre
+-- The syntax does exactly that
+SELECT tv_shows.title AS title, tsg.genre_id AS genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres AS tsg
+ON tv_shows.id = tsg.show_id
+WHERE genre_id IS NULL
+ORDER BY title, genre_id;
