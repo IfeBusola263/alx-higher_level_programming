70437f3d68d714a17487a094835aec3164ad36d6
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-17 04:55:31 +0000

    List all shows linked to genre

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- The scripts displays all the shows in the database linked to a genre
+-- The syntax does exactly that
+SELECT tv_shows.title AS title, tsg.genre_id AS genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres AS tsg
+ON tv_shows.id = tsg.show_id
+ORDER BY title, genre_id;
