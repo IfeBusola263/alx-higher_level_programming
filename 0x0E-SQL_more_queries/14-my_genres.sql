94f3928290ed27772a8f69462b6607130211e9fb
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-17 06:23:19 +0000

    Query from table tv_genres through table tv_shows with title dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- Lists all genres linked to one show "Dexter"
+-- This syntax does exactly that
+SELECT DISTINCT name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE title = "Dexter"
+ORDER BY name;
