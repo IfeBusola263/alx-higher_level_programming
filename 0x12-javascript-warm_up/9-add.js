67e452b87424441bb20feaabd312bc08a6462750
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-11 23:43:41 +0000

    Function to add numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const a = process.argv[2];
-const b = process.argv[3];
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
 
 function add (a, b) {
   console.log(`${a + b}`);
