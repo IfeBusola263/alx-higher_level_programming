79e8296404fd031b6ea55d6c2385395b4e6d3ca5
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-12 22:19:43 +0000

    print squares

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const Squ = require('./5-square.js');
+
+module.exports = class Square extends Squ {
+  // Check if c is defined
+  charPrint (c) {
+    if (c === undefined) {
+      // if c is not defined the character is changed
+      c = 'X';
+    }
+
+    // loop to print the square by binding the size to this module
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  }
+};
