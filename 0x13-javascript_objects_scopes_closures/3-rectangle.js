8695ef2d83bb5dfdbef7edee3ad090448b32ae7f
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-12 21:23:32 +0000

    Print Method implemented

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,7 +3,18 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 1 && h > 1) {
-     this.width = w;
-     this.height = h;
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const block = 'X';
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(block);
+      }
+      console.log();
+    }
   }
 };
