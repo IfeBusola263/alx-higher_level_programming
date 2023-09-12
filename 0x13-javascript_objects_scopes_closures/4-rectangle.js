674cbbc6f23303ea1ddb02e9214a4e01a784b82a
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-12 21:30:41 +0000

    Implemented double and rotate methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 1 && h > 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const block = 'X';
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(block);
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
