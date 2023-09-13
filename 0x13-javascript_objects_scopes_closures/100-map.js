39c15ed91f28dfae35c86353e832814c1b760fe7
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-13 08:04:49 +0000

    Corrected the Map task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-const newlist = list.map((num) => num * list.indexOf(num));
+let i = 0;
+const newlist = list.map((num) => num * i++);
 console.log(newlist);
