0920febbd072da1b76d2258c3966eea7400d330e
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-14 23:13:31 +0000

    Safe from injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+This module relates with a RDMS, with MySQLdb module.
+'''
+
+import MySQLdb
+import sys
+
+username = sys.argv[1]
+pwd = sys.argv[2]
+dBase = sys.argv[3]
+arg = sys.argv[4]
+
+dataBase = MySQLdb.connect(host='localhost', port=3306,
+                           user=username, passwd=pwd, db=dBase)
+cur = dataBase.cursor()
+cur.execute("SELECT id, name FROM states WHERE name LIKE %s ORDER BY id", (arg,))
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
