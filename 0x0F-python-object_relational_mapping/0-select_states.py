665830ba7341e64436fd56970808a70c203d083c
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-14 22:40:59 +0000

    State query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,9 @@ import sys
 
 username = sys.argv[1]
 pwd = sys.argv[2]
-dBase = sys.argv[2]
+dBase = sys.argv[3]
 
-dataBase = mySQLdb.connect(host='localhost:3306',
+dataBase = MySQLdb.connect(host='localhost', port=3306,
                            user=username, passwd=pwd, db=dBase)
 cur = dataBase.cursor()
 cur.execute("SELECT id, name FROM states ORDER BY id")
