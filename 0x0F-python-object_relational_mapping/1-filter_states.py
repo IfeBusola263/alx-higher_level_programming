be499ae8d38242be98409495cd63812f666b7454
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-14 22:57:41 +0000

    Query for states that starts with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+This module relates with a RDMS, with MySQLdb module.
+'''
+
+import MySQLdb
+import sys
+
+username = sys.argv[1]
+pwd = sys.argv[2]
+dBase = sys.argv[3]
+
+dataBase = MySQLdb.connect(host='localhost', port=3306,
+                           user=username, passwd=pwd, db=dBase)
+cur = dataBase.cursor()
+cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
