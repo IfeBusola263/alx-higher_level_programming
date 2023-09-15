124f0c20c42681cc5385a14451a8df219bdaa1c1
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-15 09:26:32 +0000

    Corrected the filter code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,19 +3,22 @@
 This module relates with a RDMS, with MySQLdb module.
 '''
 
-import MySQLdb
-import sys
 
-username = sys.argv[1]
-pwd = sys.argv[2]
-dBase = sys.argv[3]
-arg = sys.argv[4]
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-dataBase = MySQLdb.connect(host='localhost', port=3306,
-                           user=username, passwd=pwd, db=dBase)
-cur = dataBase.cursor()
-cur.execute("SELECT id, name FROM states WHERE name LIKE %s ORDER BY id", (arg,))
-rows = cur.fetchall()
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+    dBase = sys.argv[3]
+    arg = sys.argv[4]
 
-for row in rows:
-    print(row)
+    dataBase = MySQLdb.connect(host='localhost', port=3306,
+                               user=username, passwd=pwd, db=dBase)
+    cur = dataBase.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name \
+    LIKE %s ORDER BY id", (arg,))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
