ce3945f29f482d8671907949e47abdafed0183ba
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-15 09:22:25 +0000

    Fix name is main part

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,18 +3,20 @@
 This module relates with a RDMS, with MySQLdb module.
 '''
 
-import MySQLdb
-import sys
 
-username = sys.argv[1]
-pwd = sys.argv[2]
-dBase = sys.argv[3]
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-dataBase = MySQLdb.connect(host='localhost', port=3306,
-                           user=username, passwd=pwd, db=dBase)
-cur = dataBase.cursor()
-cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id")
-rows = cur.fetchall()
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+    dBase = sys.argv[3]
 
-for row in rows:
-    print(row)
+    dataBase = MySQLdb.connect(host='localhost', port=3306,
+                               user=username, passwd=pwd, db=dBase)
+    cur = dataBase.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
