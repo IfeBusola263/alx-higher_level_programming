9c5c3f9c113518c21a9a05819e786e9bef299742
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-15 09:19:59 +0000

    added __name__ eqauls __main__ instruction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     dBase = sys.argv[3]
 
     dataBase = MySQLdb.connect(host='localhost', port=3306,
-                           user=username, passwd=pwd, db=dBase)
+                               user=username, passwd=pwd, db=dBase)
     cur = dataBase.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id")
     rows = cur.fetchall()
