25d8069490d982de3c002480806d389bd69e5144
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-15 15:42:42 +0000

    query for first item in the table

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+This module prints all the states in the database using an ORM
+'''
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sys import argv
+    import sqlalchemy
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    username = argv[1]
+    pwd = argv[2]
+    dBase = argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        username, pwd, dBase))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+
+    if state is not None:
+        print(f'{state.id}: {state.name}')
