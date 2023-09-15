f16618814bdbb17c8b79c22c341dae4ca85a880b
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-15 10:09:57 +0000

    Added parenthesis to the format of the WHERE method

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
                                user=username, passwd=pwd, db=dBase)
     cur = dataBase.cursor()
     cur.execute("SELECT id, name FROM states WHERE name\
-    LIKE 'N%' ORDER BY id ASC")
+    LIKE ('N%') ORDER BY id ASC")
     rows = cur.fetchall()
 
     for row in rows:
