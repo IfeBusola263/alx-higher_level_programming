417776fe9dc52354b522d3d61568f800726df3b8
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-15 16:12:36 +0000

    Quary for states containing letter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+This module prints all the states in the database using an ORM
+'''
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sys import argv
+    import sqlalchemy
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    username = argv[1]
+    pwd = argv[2]
+    dBase = argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        username, pwd, dBase))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.
+                                         like('%a%')).order_by(State.id).all()
+
+    for state in states:
+        print(f'{state.id}: {state.name}')
