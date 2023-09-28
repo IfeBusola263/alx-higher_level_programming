3a81770271cd51105f4abe6abda52a6623c930d3
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-28 16:41:57 +0000

    print allowed methods on HTTP

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script displays all the allowed method by the server
-curl -s -I -X "OPTIONS" "$1" | grep "Allow" | cut -d ":" -f 1
+curl -s -I -X "OPTIONS" "$1" | grep "Allow" | awk -F ": " '{print $2}'
