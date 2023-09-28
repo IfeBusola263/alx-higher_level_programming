2a8b224cc51135c9f0f36d1063143e998239ca10
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-28 18:56:03 +0000

    Peak program for python unsorted list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module implements a function to find the peak of a list
+"""
+
+def find_peak(list_of_integers):
+    '''
+    The function finds the peak of a list
+    '''
+
+    if list_of_integers is not None and len(list_of_integers) != 0:
+        if len(list_of_integers) % 2 == 0:
+            return max(max(list_of_integers[:2]), max(list_of_integers[-2:-1]))
+        else:
+            return max(list_of_integers[-2:-1])
+
+    else:
+        return None
