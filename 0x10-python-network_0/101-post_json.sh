7ec4630ce6df965c455efcf36e8e1e5f7ce7d2b1
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-28 17:32:15 +0000

    Posts Json content

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sents a Post request and displays the body request, with the content of a file
-curl -s -L -d @"$2" "$1"
+curl -s -H "Content-Type: application/json" -d @"$2" "$1"
