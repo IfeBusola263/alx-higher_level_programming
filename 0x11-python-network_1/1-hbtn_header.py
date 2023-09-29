7d3bda508fc199b84d698d709452a828c1fd8f70
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-29 16:22:26 +0000

    fixed name is main issue

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,11 +3,9 @@
 This module implements a request to print a header variable
 of the response
 """
-
 if __name__ == '__main__':
     import urllib.request
     import sys
-
 
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.headers['X-Request-Id'])
