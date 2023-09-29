fc4c9691c606d02a33859d40c67a9b65fd4eab27
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-29 15:26:59 +0000

    fetch git hub status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module uses the urllib.request package to make an http request
+"""
+
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    msg = response.msg
+    form = response.read()
+    print('Body response:')
+    print('\t- type:{}\n\t- content: {}\n\t- utf8 content: {}'.format(
+        type(form), form, msg))
