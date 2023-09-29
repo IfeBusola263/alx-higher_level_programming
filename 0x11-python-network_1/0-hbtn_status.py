9b991d5359ec1e518c6d9ff5ba30e6367ec2ab7f
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-29 15:35:10 +0000

    fetch git hub status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,5 +9,8 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     msg = response.msg
     form = response.read()
     print('Body response:')
-    print('\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'.format(
-        type(form), form, msg))
+    # print('\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'.format(
+    #     type(form), form, msg))
+    print(f'\t- type: {type(form)}')
+    print(f'\t- content: {form}')
+    print(f'\t- utf8 content: {msg}')
