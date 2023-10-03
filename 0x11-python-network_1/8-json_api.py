fe0049a59deea4664086fdbead2135cd688e0ede
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-03 18:56:50 +0000

    correct jason code

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -23,5 +23,6 @@ if __name__ == '__main__':
         else:
             print('No result')
 
-    except requests.exceptions.JSONDecodeError as e:
+    #except requests.exceptions.JSONDecodeError as e:
+    except json.decoder.JSONDecodeError as e:
         print('Not a valid JSON')
