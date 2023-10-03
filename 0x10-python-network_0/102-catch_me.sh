9870be7c5c2344923611f32e6411c6549c24bac0
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-03 19:14:08 +0000

    Catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request to the server to display catch me if you can
-curl -s -L "$1"
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
