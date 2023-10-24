248736758f0b26035364e062829ea9ba5c81032c
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-24 12:44:38 +0000

    fixed indentations

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,11 @@
 // import the file system module
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log(data);
-	console.log();
-    }
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+    console.log();
+  }
 });
