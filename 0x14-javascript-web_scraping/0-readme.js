5b5ba5f0ac357a418caf1746c84282688a34b19d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-24 12:40:25 +0000

    Script to read from a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// import the file system module
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+    if (err) {
+	throw err;
+    } else {
+	console.log(data);
+	console.log();
+    }
+});
