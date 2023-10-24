678e56afa9f99d85baae48d2cfd9f03fff397e4b
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-24 13:08:41 +0000

    request for api

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@
 const request = require('request');
 
 request(process.argv[2], (err, response, body) => {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log("code: ${response.statusCode}");
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
