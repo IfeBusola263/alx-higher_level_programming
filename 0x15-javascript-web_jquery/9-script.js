2e98900e1ba2a413e186bd1824871087a5242a8a
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-11-02 00:01:11 +0100

    The script fetches from an api and adds the data to an html DOM

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    $.get("https://hellosalut.stefanbohacek.dev/?lang=fr",
+      function(data, status) {
+	  $("DIV#hello").text(data.hello);
+      });
+});
